309326fb8468f8fb1d441263bfc3dad09a3377bf
Jeff Sharp <jeff.sharp@git>
2018-07-24 17:12:15 -0700

    Coverage for exception cases in Finalizer.

---
+++ Sharp.Disposable.Tests/FinalizerTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using FluentAssertions;
+using FluentAssertions.Extensions;
+using NUnit.Framework;
+
+namespace Sharp.Disposable
+{
+    [TestFixture]
+    public class FinalizerTests
+    {
+        [Test]
+        public void RunUntil_NullCondition()
+        {
+            this.Invoking(_ => Finalizer.RunUntil(null))
+                .Should().Throw<ArgumentNullException>();
+        }
+
+        [Test]
+        public void RunUntil_NullCondition_AnyInterval()
+        {
+            this.Invoking(_ => Finalizer.RunUntil(null, 1.Seconds()))
+                .Should().Throw<ArgumentNullException>();
+        }
+
+        [Test]
+        public void RunUntil_SomeCondition_NegativeInterval()
+        {
+            this.Invoking(_ => Finalizer.RunUntil(() => true, (-1).Seconds()))
+                .Should().Throw<ArgumentOutOfRangeException>();
+        }
+    }
+}
