c1d001dcc66aee2b58bd98bda96816df5d7bbe5f
Jeff Sharp <public@sharpjs.net>
2019-12-11 15:19:53 -0600

    Produce merged coverage report. Output coverage percent in build log.

--- Make.ps1
+++ Make.ps1
@@ -15,9 +15,13 @@ param (
     [Parameter(ParameterSetName="Test")]
     [switch] $Test,
 
-    # Build, run tests, and produce code covarage report.
+    # Build, run tests, and produce merged code coverage report.
     [Parameter(Mandatory, ParameterSetName="Coverage")]
     [switch] $Coverage,
+
+    # Produce merged code coverage report.
+    [Parameter(Mandatory, ParameterSetName="MergeCoverage")]
+    [switch] $MergeCoverage,
 
     # The configuration to build: Debug or Release.  The default is Debug.
     [Parameter(ParameterSetName="Build")]
@@ -31,7 +35,7 @@ param (
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
-$MainAssemblyName = "Sharp.Disposable"
+$AssemblyNameRoot = "Sharp.Disposable"
 
 $Command = $PSCmdlet.ParameterSetName
 if ($Command -eq "Test") { $Test = $true }
@@ -48,12 +52,21 @@ Write-Host -ForegroundColor Cyan @'
 '@
 
 function Main {
-    Invoke-Build
+    if (-not $MergeCoverage) {
+        Invoke-Build
+    }
 
     if ($Test -or $Coverage) {
-        Set-Location -LiteralPath "$MainAssemblyName.Tests"
+        Set-Location -LiteralPath "$AssemblyNameRoot.Tests"
         Invoke-TestForTargetFramework net48
         Invoke-TestForTargetFramework netcoreapp3.1
+        Set-Location ..
+    }
+
+    if ($Coverage -or $MergeCoverage) {
+        Set-Location -LiteralPath coverage
+        Export-CoverageReport
+        Set-Location ..
     }
 } 
 
@@ -71,17 +84,29 @@ function Invoke-TestForTargetFramework {
     Write-Phase "Test: $TargetFramework$(if ($Coverage) {" + Coverage"})"
     Invoke-DotNetExe -Arguments @(
         if ($Coverage) {
+            $Directory = Get-Location | Split-Path -Leaf
             "dotcover"
-                "--dcReportType=HTML"
-                "--dcOutput=..\coverage\$TargetFramework.html"
-                "--dcFilters=+:$MainAssemblyName`;+:$MainAssemblyName.*`;-:*.Tests"
-                "--dcAttributeFilters=-:System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverageAttribute"
+                "--dcOutput=..\coverage\$Directory.$TargetFramework.dcvr"
+                "--dcFilters=+:$AssemblyNameRoot`;+:$AssemblyNameRoot.*`;-:*.Tests"
+                "--dcAttributeFilters=System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverageAttribute"
         }
         "test"
             "--framework:$TargetFramework"
             "--configuration:$Configuration"
             "--no-build"
     )
+}
+
+function Export-CoverageReport {
+    Write-Phase "Coverage Report"
+    $Snapshots = (Get-Item *.dcvr) -join ';'
+    Invoke-DotCoverExe merge  /Source=$Snapshots   /Output=Coverage.mrg
+    Invoke-DotCoverExe report /Source=Coverage.mrg /Output=Coverage.html /ReportType=HTML /HideAutoProperties
+    Invoke-DotCoverExe report /Source=Coverage.mrg /Output=Coverage.xml  /ReportType=XML  /HideAutoProperties
+    $Report   = [xml] (Get-Content Coverage.xml -Raw -Encoding UTF8)
+    $Coverage = [int] $Report.Root.CoveragePercent
+    $Color    = $(if ($Coverage -eq 100) {"Green"} else {"Red"})
+    Write-Host "`nCode Coverage: $Coverage%`n" -ForegroundColor $Color
 }
 
 function Invoke-DotNetExe {
@@ -91,6 +116,26 @@ function Invoke-DotNetExe {
     )
     & dotnet.exe $Arguments
     if ($LASTEXITCODE -ne 0) { throw "dotnet.exe exited with an error." }
+}
+
+function Invoke-DotCoverExe {
+    param (
+        [Parameter(Mandatory, ValueFromRemainingArguments)]
+        [string[]] $Arguments
+    )
+    $DotCoverExe = Resolve-NuGetPackageCache `
+        | Join-Path -Resolve -ChildPath jetbrains.dotcover.dotnetclitool\2019.3.0 `
+        | Join-Path -Resolve -ChildPath tools\dotCover.exe
+    & $DotCoverExe $Arguments
+    if ($LASTEXITCODE -ne 0) { throw "dotcover.exe exited with an error." }
+}
+
+function Resolve-NuGetPackageCache {
+    $Path = Invoke-DotNetExe nuget locals global-packages --list
+    # dotnet.exe returns package path in a format that must be split up:
+    #     info : global-packages: C:\Users\Foo\.nuget\packages\
+    #                             |-------desired--part-------|
+    ($Path -split ': ')[2]
 }
 
 function Write-Phase {
